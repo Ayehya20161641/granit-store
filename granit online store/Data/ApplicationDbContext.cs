76fa540124810d8817383753a4f4c8a588bc608d
mohamedahmed <20162835@fue.edu.eg>
2019-09-02 14:31:28 +0200

    product

---
+++ granit online store/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using granitonlinestore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace granit_online_store.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+    }
+}
